1819f3eafbde18956e26c4f41ac68f09cefdd885
Antoine <antoine@enimaloc.fr>
2023-11-02 23:37:39 +0100

    build: add missing dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,15 @@ dependencies {
     implementation("fr.enimaloc.night-config:toml:3.7.0")
     implementation("org.xerial:sqlite-jdbc:3.41.0.1")
     implementation("fr.enimaloc:matcher:0.1.5")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.13.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.0")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.0")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.13.0")
+
+    implementation("com.github.imcdonagh:image4j:0.7.2")
+
+    implementation("com.theokanning.openai-gpt3-java:service:0.16.1")
+    implementation("org.apache.commons:commons-text:1.10.0")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
