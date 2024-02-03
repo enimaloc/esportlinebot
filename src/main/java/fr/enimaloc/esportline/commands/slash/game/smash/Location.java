dbaf0d48772d9c6ae6633bb777ec0261fd2b4857
Antoine <antoine@enimaloc.fr>
2024-02-03 03:19:57 +0100

    feat(game/smash): add method to get the non null field

--- src/main/java/fr/enimaloc/esportline/commands/slash/game/smash/Location.java
+++ src/main/java/fr/enimaloc/esportline/commands/slash/game/smash/Location.java
@@ -1,4 +1,15 @@
 package fr.enimaloc.esportline.commands.slash.game.smash;
 
 public record Location(String country, String state, String city, String cCountry, String cState, String cCity) {
+    public String effectiveCountry() {
+        return country == null ? cCountry : country;
+    }
+
+    public String effectiveState() {
+        return state == null ? cState : state;
+    }
+
+    public String effectiveCity() {
+        return city == null ? cCity : city;
+    }
 }
